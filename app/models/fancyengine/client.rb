3f4d5fd1872ae5bc78bf73b49b4e3fad91fd71c0
Sean Devine <barelyknown@icloud.com>
2015-07-31 16:44:12 -0400

    Use the client's request class directly instead of through goofy method.

--- app/models/fancyengine/client.rb
+++ app/models/fancyengine/client.rb
@@ -1,15 +1,17 @@
 module Fancyengine
   class Client
+    extend Forwardable
+    def_delegators :@client, :request
 
     def initialize(key = Fancyengine.key, secret = Fancyengine.secret)
       unless key.present? && secret.present?
-        raise ArgumentError, "credentials are not configured. Set Fancyengine.key and Fancyengine.secret with your Fancy Hands credentials."
+        raise ArgumentError, "Credentials are not configured. Set Fancyengine.key and Fancyengine.secret with your Fancy Hands credentials."
       end
       @client = FancyHands::V1::Client.new(key, secret)
     end
 
     def ping
-      @client.Echo.get({}) == {}
+      request.get("echo", {}) == {}
     end
 
   end
