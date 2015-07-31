bea1d3eb0a0e4dfadab84c864c3b86e57a8f7d96
Sean Devine <barelyknown@icloud.com>
2015-07-31 16:49:42 -0400

    Neaten up the client spec.

--- spec/models/fancyengine/client_spec.rb
+++ spec/models/fancyengine/client_spec.rb
@@ -1,21 +1,12 @@
 RSpec.describe Fancyengine::Client do
 
   context "when the key and secret are not configured" do
-    before do
-      Fancyengine.key = nil
-      Fancyengine.secret = nil
-    end
     it "raises a credentials not available error" do
-      expect{subject}.to raise_error(ArgumentError)
+      expect{described_class.new(nil, nil)}.to raise_error(ArgumentError)
     end
   end
 
   context "when the key and secret are configured" do
-    before do
-      Fancyengine.key = ENV["KEY"]
-      Fancyengine.secret = ENV["SECRET"]
-    end
-
     it "initializes without error" do
       expect{subject}.not_to raise_error
     end
