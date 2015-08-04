0b797b9ea4d28121fc3482574127cbd253f6b598
Sean Devine <barelyknown@icloud.com>
2015-08-04 15:19:53 -0400

    Update migrations to add messages and phone calls at the same time.

--- db/migrate/20150804180104_add_messages_to_fancyengine_custom_requests.rb
+++ db/migrate/20150804180104_add_messages_to_fancyengine_custom_requests.rb
@@ -1,9 +1,11 @@
 class AddMessagesToFancyengineCustomRequests < ActiveRecord::Migration
   def change
     add_column :fancyengine_custom_requests, :messages, :text, default: "[]"
+    add_column :fancyengine_custom_requests, :phone_calls, :text, default: "[]"
 
     reversible do |dir|
       dir.up do
+        Fancyengine.reset_column_information
         Fancyengine::CustomRequest.find_each do |custom_request|
           custom_request.save!
         end
